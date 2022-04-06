b93ccc91126a7a003d8a71fcf8bacc4131b144d9
Senali Dilumika <senali.d@fidenz.com>
2022-04-06 10:59:26 +0530

    style main body section of the home page

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,81 @@
 import React from 'react'
-import styles from 'styled-components'
+import styled from 'styled-components'
 
 const Section = () => {
   return (
     <Wrap>
-      Section
+      <ItemText>
+        <h1>Model 3</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   )
 }
 
 export default Section
 
-const Wrap = styles.div`
+const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background: orange;
+  background-position: center;
+  background-size: cover;
+  background-react: no-repeat;
+  background-image: url('/images/model-3.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const ItemText =  styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+const LeftButton = styled.div`
+  background-color: #171A20;
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+  background-color: #ffffff;
+  opacity: 0.6;
+  color: #000000;
+`
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflw-x: hidden;
+`
+
+const Buttons = styled.div`
+  
 `
