32098da5f92a9b020943d184f9e07d859ea35056
Senali Dilumika <senali.d@fidenz.com>
2022-04-06 13:04:46 +0530

    add animation for home page content

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
 
 const Section = ({ title, description, backgroundImage, leftBtnText, rightBtnText}) => {
   return (
     <Wrap bgImage={backgroundImage}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {
-            rightBtnText && <RightButton>{rightBtnText}</RightButton>
-          }
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {
+              rightBtnText && <RightButton>{rightBtnText}</RightButton>
+            }
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
